2791fd05f42f4567da5c06447586f75b77897e1e
MatthieuGriffon <postmaster@matthieu-griffon.fr>
2024-10-10 10:35:10 +0200

    Initial commit with React, Vite, and Three.js setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ThreeScene from "./components/ThreeScene";
+
+function App() {
+  return (
+    <div className="App">
+      <ThreeScene />
+    </div>
+  );
+}
+
+export default App;
